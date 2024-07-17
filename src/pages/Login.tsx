63b2a67eb9a577005f8a817a1b0947d3426daa8d
suprajasribalaji <suprajasri.balaji@gmail.com>
2024-07-17 18:21:02 +0530

    login setup for user, admin

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,138 +1,115 @@
-import React, { useState } from "react";
-import { Form, Input, Button, Typography, message, Space } from "antd";
-import styled from "styled-components";
-import { auth } from "../config/firebase.config";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
-import { Buttons } from "../components/themes/color";
+import { useState } from 'react';
+import { firestore } from '../config/firebase.config';
+import { collection, addDoc, getDocs, query, where } from 'firebase/firestore';
+import { Button, Form, Modal, Tabs, message } from 'antd';
+import styled from 'styled-components';
+import Title from 'antd/es/typography/Title';
+import LoginForm from '../components/form/LoginForm';
 
-const { Title } = Typography;
+const { TabPane } = Tabs;
 
-const Login: React.FC = () => {
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
+const UserLogin = () => {
+  const [open, setOpen] = useState(false);
+  const [form] = Form.useForm();
 
-  const onFinish = async (values: any) => {
-    setLoading(true);
-    try {
-      await signInWithEmailAndPassword(auth, values.email, values.password);
-      message.success("Logged in Successfully");
-      navigate("/home");
-    } catch (error) {
-      console.error(error);
-      setLoading(false);
-      message.error("Please Enter Valid Credentials.");
+  const AddNewuser = async (values: any) => {
+    const { username, password, email } = values;
+    if (values) {
+      const collectionRef = collection(firestore, 'user');
+      const querySnapshot = await getDocs(query(collectionRef, where('email', '==', email)));
+
+      if (!querySnapshot.empty) {
+        message.error('Already an user! Use different email-id!!');
+        return;
+      }
+
+      try {
+        const res = await addDoc(collectionRef, {
+          user_id: username,
+          email: email,
+          password: password,
+        });
+        console.log('User added successfully!', res);
+        message.success('Account created successfully! Please login to continue');
+        form.resetFields();
+      } catch (error: any) {
+        console.error('Error adding user: ', error);
+        message.error('Account creation failed! Please try again by filling all fields correctly');
+      }
+      setOpen(false);
     }
-  };
-
-  const handleSignUp = () => {
-    navigate("/signup");
   };
 
   return (
     <StyledContainer>
       <StyledFormContainer>
-        <Title level={2} style={{ textAlign: "center" }}>
+        <Title level={2} style={{ textAlign: 'center' }}>
           LOGIN
         </Title>
-        <Form
-          name="login"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          layout="vertical"
-        >
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[
-              { required: true, message: "Please input your email!" },
-              {
-                type: "email",
-                message: "Please enter a valid email address!",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[
-              { required: true, message: "Please input your password!" },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-
-          <Form.Item>
-            <Space direction="horizontal" size="middle">
-              <StyledButton
-                type="link"
-                htmlType="submit"
-                block
-                loading={loading}
+        <Tabs defaultActiveKey="1">
+          <TabPane tab="User" key="1">
+            <LoginForm
+              form={form} // Pass form instance
+              collectionName="user"
+              successMessage="User logged in successfully"
+              redirectPath="/home"
+              formType="login"
+            />
+            <Form.Item>
+              Already a user?
+              <Button type="link" onClick={() => setOpen(true)}>
+                Create One!
+              </Button>
+              <Modal
+                visible={open}
+                title="Create New Account!"
+                onCancel={() => setOpen(false)}
+                footer={null}
               >
-                IN AS USER
-              </StyledButton>
-              <StyledButton
-                type="link"
-                htmlType="submit"
-                block
-                loading={loading}
-              >
-                IN AS ADMIN
-              </StyledButton>
-            </Space>
-          </Form.Item>
-        </Form>
-        <SignupButton>
-          <Space>
-          <InfoAboutSignup>Don't have an account?</InfoAboutSignup>  
-          <StyledSignupButton type="link" onClick={handleSignUp}>Sign Up</StyledSignupButton>
-          </Space>
-          </SignupButton>
+                <LoginForm
+                  form={form} // Pass form instance
+                  collectionName="user"
+                  successMessage="User registered successfully"
+                  redirectPath="/home"
+                  formType="register"
+                  onRegister={AddNewuser}
+                />
+              </Modal>
+            </Form.Item>
+          </TabPane>
+          <TabPane tab="Admin" key="2">
+            <LoginForm
+              form={form} // Pass form instance
+              collectionName="admin"
+              successMessage="Admin logged in successfully"
+              redirectPath="/home"
+              formType="login"
+            />
+          </TabPane>
+        </Tabs>
       </StyledFormContainer>
     </StyledContainer>
   );
 };
 
-export default Login;
+export default UserLogin;
 
 const StyledContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   min-height: 100vh;
-  min-width: 100%;
 `;
 
 const StyledFormContainer = styled.div`
-  width: 22%;
-`;
-
-const StyledButton = styled(Button)`
-  background-color: ${Buttons.backgroundColor};
-  color: ${Buttons.text};
-  border: none;
-  &&&:hover {
-    color: ${Buttons.hover};
-  }
-`;
-
-const SignupButton = styled.div`
+  width: 100%;
+  max-width: 400px;
+  padding: 20px;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
   display: flex;
-  margin-left: 8.5%;
-`;
-
-const InfoAboutSignup = styled.div`
-  font-family: sans-serif;
-`;
-
-const StyledSignupButton = styled(Button)`
-  border: none;
-  &&&:hover {
-    color: ${Buttons.hover}
-  }
-  color: ${Buttons.backgroundColor}
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
